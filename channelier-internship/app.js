962c54abfb897da3ae9be9c17bbad03fd95caef9
Vaib215 <21bcs10561@cuchd.in>
2022-10-22 10:06:02 +0530

    Final Fix: Added Text Animations

--- channelier-internship/app.js
+++ channelier-internship/app.js
@@ -1,5 +1,5 @@
 const particlesJS = window.particlesJS;
-particlesJS.load('particles-js', 'particlesjs-config.json', function () {
+particlesJS.load('particles-js', 'particlesjs-config.json', ()=> {
     console.log('callback - particles.js config loaded');
 });
 
@@ -18,11 +18,23 @@ const intersectionObserver = new IntersectionObserver(entries => {
     });
 }, {
     threshold: 0.5
-    });
+});
 intersectionObserver.observe(benefitContainer);
 
+
+const awardCat = document.querySelectorAll(".award-cat");
+let i = 1;
+setInterval(() => {
+    awardCat.forEach(cat => {
+        cat.classList.remove("active");
+    })
+    awardCat[i].classList.add("active");
+    i++;
+    i = i % 3;
+},5000);
+
 const particle = document.querySelector("#particles-js");
-window.addEventListener("mousemove",e=>{
-    particle.style.left = (e.clientX-200)/15+"px";
-    particle.style.top = (e.clientY-200)/15+"px";
+window.addEventListener("mousemove", e => {
+    particle.style.left = (e.clientX - 200) / 15 + "px";
+    particle.style.top = (e.clientY - 200) / 15 + "px";
 })
